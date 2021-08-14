fc2a81b3cc87933d93997091af8c9899626c1f65
Jupiter1206 <jupiter1206jove@gmail.com>
2021-08-14 08:03:45 +0300

    lotwork

--- src/views/Lot.vue
+++ src/views/Lot.vue
@@ -2,7 +2,7 @@
   <div class="page-lot">
     <div class="title">Choose your lot</div>
     <div id="gmapdiv">
-      <iframe id="lwmap" src="https://maps.lotworks.ca/maps/sterlingedm/sienna/build" style="border: 0; width: 80%; height: 100%;"></iframe> 
+      <iframe id="lwmap" src="https://maps.lotworks.ca/maps/chatterson/legacy" style="border: 0; width: 80%; height: 100%;"></iframe> 
     </div>
   </div>
 </template>
@@ -10,4 +10,4 @@
 <script>
 export default {
 }
-</script>
+</script> 
