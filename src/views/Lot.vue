591ba42a04819d04001c21fe66bb1a454321bb18
Jupiter1206 <jupiter1206jove@gmail.com>
2021-08-14 08:27:16 +0300

    lotwork-2

--- src/views/Lot.vue
+++ src/views/Lot.vue
@@ -2,7 +2,7 @@
   <div class="page-lot">
     <div class="title">Choose your lot</div>
     <div id="gmapdiv">
-      <iframe id="lwmap" src="https://maps.lotworks.ca/maps/chatterson/legacy" style="border: 0; width: 80%; height: 100%;"></iframe> 
+      <iframe id="lwmap" src="https://www.sterlingedmonton.com/lots/lotworks/?status=available&buildpocket=22&frontage=27.26&producttype=Single%20Family%20-%20ZLL%20Front%20Garage&stdproducttype=Single%20Family%20Front%20-%20Zero%20Lot%20Line&lkp_stdproducttype=4&lotaddress=&lot=35&block=1&phase=14C&community=Edgemont%20East" style="border: 0; width: 100%; height: 100%;"></iframe> 
     </div>
   </div>
 </template>
